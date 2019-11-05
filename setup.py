e2226dfe82327cfec0c25ffbf9357a36633a14c4
tadgh <garygrantgraham@gmail.com>
2019-11-05 15:48:18 -0800

    Add forgotten bump to 0.2.0

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/Kaniwani/wanikani_api",
-    version="version='0.1.2'",
+    version="version='0.2.0'",
     zip_safe=False,
 )
