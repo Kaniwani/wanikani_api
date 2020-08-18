2cf4eaf97082d6e6a72ccdb233030ecc7dc3fa6c
Tadgh <garygrantgraham@gmail.com>
2020-08-18 15:28:39 -0700

    Bump version: 0.5.0 → 0.5.1

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/Kaniwani/wanikani_api",
-    version="0.5.0",
+    version="version='0.5.1'",
     zip_safe=False,
 )
